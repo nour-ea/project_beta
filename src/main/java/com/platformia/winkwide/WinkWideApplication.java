d856042869ab5fc2ef876c187c5413ae702a1453
nour <nour@nours-mbp>
2018-06-20 17:43:46 +0100

    first commit

---
+++ src/main/java/com/platformia/winkwide/WinkWideApplication.java
@@ -0,0 +1,12 @@
+package com.platformia.winkwide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinkWideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinkWideApplication.class, args);
+	}
+}
